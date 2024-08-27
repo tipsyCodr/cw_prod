04294e81c67739b780a57766987ad19c58d69157
Hemant Kumar <itshemant566@gmail.com>
2024-08-27 14:23:34 +0530

    fixed link

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://pathideamultiskill.com/cw/';
+$config['base_url'] = 'https://pathideamultiskill.com/cw_prod/';
 
 /*
 |--------------------------------------------------------------------------
