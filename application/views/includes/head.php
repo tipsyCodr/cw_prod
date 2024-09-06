2f7633c45fc98d2f3cbbbd869bdcef28f5b0eba1
Hemant Kumar <itshemant566@gmail.com>
2024-09-06 13:25:53 +0530

    ui Changes

--- application/views/includes/head.php
+++ application/views/includes/head.php
@@ -1,8 +1,10 @@
-<div class="p-1 flex flex-row justify-between">
+<div class="p-1 flex flex-row justify-between items-center">
     <div class=" img-wrapper">
         <a href="#"><img src="<?= base_url() . 'assets/images/icons/menu.png' ?>" alt="menu" width="50px"></a>
     </div>
-    <div class="logo">
-        <img src="<?= base_url() . 'assets/images/logo.png' ?>" width="50px" alt="Logo">
+    <div class="user p-4 flex flex-row">
+        <a href="#" class="p-2"><img src="<?= base_url() . 'assets/images/icons/chat.png' ?>" width="30px" alt=""></a>
+        <a href="#" class="p-2"><img src="<?= base_url() . 'assets/images/icons/user.png' ?>" width="30px"
+                alt="Logo"></a>
     </div>
 </div>
--- application/views/includes/nav.php
+++ application/views/includes/nav.php
@@ -19,40 +19,42 @@
     <div class="wrapper ">
         <ul class="flex text-center p-2 justify-evenly items-center pb-4">
             <li><a href="<?= base_url('/') ?>" class=" nav-item w-full text-decoration-none ">
-                    <!-- <img src="<?= base_url('assets/images/icons/home.svg') ?>" alt="home icon" width="30em"
-                        height="30em" /> -->
-                    <i
-                        class="fas fa-2x fa-home <?= (current_url() === base_url('/')) ? 'text-activeItem active' : '' ?>"></i>
+                    <img src="<?= base_url('assets/images/icons/home-page.png') ?>" alt="home icon" width="30em"
+                        height="30em" />
+                    <!-- <i
+                        class="fas fa-2x fa-home <?= (current_url() === base_url('/')) ? 'text-activeItem active' : '' ?>"></i> -->
                 </a></li>
             <li><a href="<?= base_url('services') ?>" class=" nav-item w-full text-decoration-none">
-                    <!-- <img src="<?= base_url('assets/images/icons/service.svg') ?>" alt="services icon" width="33em"
-                        height="31em" /> -->
-                    <i
+                    <img src="<?= base_url('assets/images/icons/customer-line.png') ?>" alt="services icon" width="33em"
+                        height="31em" />
+                    <!-- <i
                         class="fa fa-2x fa-suitcase <?= (current_url() === base_url('services')) ? 'text-activeItem active' : '' ?>">
-                    </i>
+                    </i> -->
 
                 </a></li>
             <li><a href="<?= base_url('post') ?>" class=" nav-item w-full text-decoration-none">
-                    <!-- <img src="<?= base_url('assets/images/icons/post.svg') ?>" alt="post icon" width="55em" height="55em" /> -->
+                    <img src="<?= base_url('assets/images/icons/super.png') ?>" alt="post icon" width="55em"
+                        height="55em" />
 
-                    <i
+                    <!-- <i
                         class="fa-solid fa-3x fa-square-plus <?= (current_url() === base_url('post')) ? 'text-activeItem active' : '' ?>">
-                    </i>
+                    </i> -->
                 </a>
             </li>
             <li><a href="<?= base_url('matrimonial') ?>" class=" nav-item w-full text-decoration-none">
-                    <!-- <img src="<?= base_url('assets/images/icons/matrimonial.svg') ?>" alt="matrimony icon" width="38em"
-            height="38em" /> -->
-                    <i
+                    <img src="<?= base_url('assets/images/icons/relationship-line.png') ?>" alt="matrimony icon"
+                        width="38em" height="38em" />
+                    <!-- <i
                         class="fa-brands fa-2x fa-gratipay <?= (current_url() === base_url('matrimonial')) ? 'text-activeItem active' : '' ?>">
-                    </i>
+                    </i> -->
 
                 </a></li>
             <li><a href="<?= base_url('profile') ?>" class="nav-item w-full text-decoration-none">
-                    <!-- <img src="<?= base_url('assets/images/icons/profile.svg') ?>" alt="profile icon" width="37em" height="37em" /> -->
-                    <i
+                    <img src="<?= base_url('assets/images/icons/user.png') ?>" alt="profile icon" width="37em"
+                        height="37em" />
+                    <!-- <i
                         class="fa fa-2x fa-user <?= (current_url() === base_url('profile')) ? 'text-activeItem active' : '' ?>">
-                    </i>
+                    </i> -->
                 </a>
             </li>
         </ul>
