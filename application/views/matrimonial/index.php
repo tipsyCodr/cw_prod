36158d48a22dfe63c63cad45b3f17519745d341e
Hemant Kumar <itshemant566@gmail.com>
2024-09-07 19:16:41 +0530

    Matrimonial form

--- application/views/matrimonial/index.php
+++ application/views/matrimonial/index.php
@@ -2,37 +2,48 @@
     <!-- <h1>Matrimonial</h1> -->
     <h2 class="font-bold text-3xl text-center">Find Your Perfect Pair</h2>
     <div class="matrimonial-wrapper grid grid-cols-2 gap-4 py-6 px-4">
+
         <div class="col-span-1">
-            <a href="<?= base_url('matrimonial') ?>">
-                <div class="matrimonial-card">
-                    <div class="img-wrapper  overflow-hidden">
-                        <img class="img-fluid rounded-lg transition-all hover:scale-90"
-                            src="<?= base_url('assets/images/bride.jpg') ?>" alt="">
-                    </div>
-                    <div class="matrimonial-card-body">
-                        <p class="font-bold text-xl ">Bride</p>
-                    </div>
+            <button type="submit" form="form_bride" formaction="<?= base_url('matrimonial_find_match') ?>"
+                class="matrimonial-card">
+                <div class="img-wrapper  overflow-hidden">
+                    <img class="img-fluid rounded-lg transition-all hover:scale-90"
+                        src="<?= base_url('assets/images/bride.jpg') ?>" alt="">
                 </div>
-            </a>
+                <div class="matrimonial-card-body">
+                    <p class="font-bold text-xl ">Groom</p>
+                </div>
+            </button>
         </div>
         <div class="col-span-1">
-            <a href="<?= base_url('matrimonial') ?>">
-                <div class="matrimonial-card">
-                    <div class="img-wrapper  overflow-hidden">
-                        <img class="img-fluid rounded-lg transition-all hover:scale-90"
-                            src="<?= base_url('assets/images/groom.jpg') ?>" alt="">
-                    </div>
-                    <div class="matrimonial-card-body">
-                        <p class="font-bold text-xl ">Groom</p>
-                    </div>
+            <button type="submit" form="form_groom" formaction="<?= base_url('matrimonial_find_match') ?>"
+                class="matrimonial-card">
+                <div class="img-wrapper  overflow-hidden">
+                    <img class="img-fluid rounded-lg transition-all hover:scale-90"
+                        src="<?= base_url('assets/images/groom.jpg') ?>" alt="">
                 </div>
-            </a>
+                <div class="matrimonial-card-body">
+                    <p class="font-bold text-xl ">Groom</p>
+                </div>
+            </button>
         </div>
 
+        <form action="<?= base_url('matrimonial_find_match') ?>" method="POST" id="form_groom">
+            <input type="hidden" name="looking" value="M">
+            <input type="hidden" name="from_age" value="18">
+            <input type="hidden" name="to_age" value="65">
 
+        </form>
+
+        <form action="<?= base_url('matrimonial_find_match') ?>" method="POST" id="form_bride">
+            <input type="hidden" name="looking" value="F">
+            <input type="hidden" name="from_age" value="18">
+            <input type="hidden" name="to_age" value="65">
+
+        </form>
 
     </div>
-    <div class="search-wrapper mx-auto max-w-md px-4 py-6 space-y-4 bg-white rounded-lg shadow-lg">
+    <div class="search-wrapper mx-auto max-w-md px-4 py-6 space-y-4 bg-white rounded-lg ">
         <form action="<?= base_url('matrimonial_find_match') ?>" method="POST" enctype="multipart/form-data">
             <div class="form-group flex flex-row space-x-2">
                 <div class="flex-1 ">
@@ -68,4 +79,12 @@
             </div>
         </form>
     </div>
+
+    <div class="mb-8 mt-2 text-center">
+        <a href="<?= base_url('matrimonial_form') ?>" class="text-indigo-600 hover:text-indigo-900 font-semibold">
+            Don't have a Matrimonial account? Create one now
+        </a>
+    </div>
+
+
 </div>
