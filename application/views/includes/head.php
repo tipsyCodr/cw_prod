3d047cfa75b3dc3dfd62a732f45390a20749110b
Hemant Kumar <itshemant566@gmail.com>
2024-09-10 13:30:25 +0530

    sd

--- application/views/includes/head.php
+++ application/views/includes/head.php
@@ -1,11 +1,11 @@
 <div class="p-1 flex flex-row justify-between items-center bg-blue-500">
     <div class=" flex img-wrapper items-center">
-        <a href="#"><img class='m-2' src="<?= base_url() . 'assets/images/logo.png' ?>" alt="menu" width="50px"></a>
+        <a href="#"><img class='m-0' src="<?= base_url() . 'assets/images/logo.png' ?>" alt="menu" width="50px"></a>
         <p class="px-1 font-black text-white text-2xl uppercase text-nowrap">
             Patel Samaaj
         </p>
     </div>
-    <div class="user p-4 flex flex-row justify-evenly">
+    <div class="user p-14 flex flex-row justify-evenly">
         <a href="#" class="p-1 m-1  bg-white rounded-full"><img src="<?= base_url() . 'assets/images/icons/chat.png' ?>"
                 width="30px" alt=""></a>
         <?php if ($this->session->userdata('login')): ?>
