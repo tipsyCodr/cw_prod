09fb31bc802565d2a2e803718c512591d23ec091
Hemant Kumar <itshemant566@gmail.com>
2024-09-10 17:42:40 +0530

    sticky navbar

--- application/views/includes/head.php
+++ application/views/includes/head.php
@@ -1,19 +1,44 @@
-<div class="p-1 flex flex-row justify-between items-center bg-accent">
-    <div class=" flex img-wrapper items-center">
-        <a href="#"><img class='m-0' src="<?= base_url() . 'assets/images/logo.png' ?>" alt="menu" width="50px"></a>
-        <p class="px-1 font-light text-white text-xl uppercase text-nowrap">
-            Patel Samaj
-        </p>
-    </div>
-    <div class="user p-0 flex flex-row justify-evenly">
-        <a href="#" class="p-1 px-1.5 m-1  bg-white rounded-full"><img
-                src="<?= base_url() . 'assets/images/icons/chat.png' ?>" width="27px" alt=""></a>
-        <?php if ($this->session->userdata('login')): ?>
-            <a href="<?= base_url('logout') ?>" class="p-1 m-1 bg-white rounded-full "> <i
-                    class="fa-solid fa-right-from-bracket fa-2x"></i></a>
-        <?php else: ?>
-            <a href="<?= base_url('login') ?>" class="p-1 m-1 bg-white rounded-full"><img
-                    src="<?= base_url() . 'assets/images/icons/user.png' ?>" width="30px" alt="Logo"></a>
-        <?php endif; ?>
+<div class="sticky-header">
+    <div class="p-1 flex flex-row justify-between items-center bg-accent">
+        <div class=" flex img-wrapper items-center">
+            <a href="#"><img class='m-0' src="<?= base_url() . 'assets/images/logo.png' ?>" alt="menu" width="50px"></a>
+            <p class="px-1 font-light text-white text-xl uppercase text-nowrap">
+                Patel Samaj
+            </p>
+        </div>
+        <div class="user p-0 flex flex-row justify-evenly">
+            <a href="#" class="p-1 px-1.5 m-1  bg-white rounded-full"><img
+                    src="<?= base_url() . 'assets/images/icons/chat.png' ?>" width="27px" alt=""></a>
+            <?php if ($this->session->userdata('login')): ?>
+                <a href="<?= base_url('logout') ?>" class="p-1 m-1 bg-white rounded-full "> <i
+                        class="fa-solid fa-right-from-bracket fa-2x"></i></a>
+            <?php else: ?>
+                <a href="<?= base_url('login') ?>" class="p-1 m-1 bg-white rounded-full"><img
+                        src="<?= base_url() . 'assets/images/icons/user.png' ?>" width="30px" alt="Logo"></a>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
+
+<script>
+    window.onscroll = function () { myFunction() };
+
+    var header = document.querySelector(".sticky-header");
+    var sticky = header.offsetTop + 50;
+
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+</script>
+
+<style>
+    .sticky {
+        position: fixed;
+        top: 0;
+        width: 100%;
+    }
+</style>
