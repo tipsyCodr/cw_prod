71adc75b8ca68716c4c0416911fa4cb4656a4c48
Hemant Kumar <itshemant566@gmail.com>
2024-09-11 13:35:55 +0530

    Added Firebase and google signin TODO: need to authenticate the user after google success signin

--- application/views/login.php
+++ application/views/login.php
@@ -26,4 +26,60 @@
                 class="p-2  inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
                 href="<?php echo base_url('forgot_password') ?>">Forgot Password?</a></div>
     </form>
+    <div class="flex justify-center">
+        <div class="col-sm-4">
+            <button type="button" id="google-login" name="google-login"
+                class="p-2 font-bold text-lg bg-red-500 text-white rounded"><i class="fa-brands fa-google mx-2"></i>
+                Login with Google</button>
+        </div>
+    </div>
 </div>
+<script type="module">
+    // Import the functions you need from the SDKs
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
+    import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyCK8tduZqTnJRS2xHcQfRflNQW4zR_nQK0",
+        authDomain: "community-website-7757e.firebaseapp.com",
+        projectId: "community-website-7757e",
+        storageBucket: "community-website-7757e.appspot.com",
+        messagingSenderId: "63119085208",
+        appId: "1:63119085208:web:ebdbc057fe91b7f82e4aed",
+        measurementId: "G-SRMNFHMN7H"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
+    // Initialize Firebase Authentication and set up Google provider
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    console.log("Provider: " + provider);
+
+    // Event listener for Google login
+    document.getElementById("google-login").addEventListener("click", function () {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                alert("Welcome " + user.displayName);
+
+                console.log(user);
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log("Error: " + errorMessage);
+                const email = error.customData.email;
+                const credential = GoogleAuthProvider.credentialFromError(error);
+            });
+    });
+</script>
---
+++ assets/js/login.js
@@ -0,0 +1,3 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
