38e16ab9a3ef0b94a758e8eae06fe0db751bfd6e
Hemant Kumar <itshemant566@gmail.com>
2024-09-17 19:56:30 +0530

    asds

--- application/views/about.php
+++ application/views/about.php
@@ -1,38 +1,44 @@
-<div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-1">
-    <a href="<?= base_url('about-samaj') ?>"
-        class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
-        <i class="fas fa-users text-4xl"></i>
-        <h3 class="text-xl font-semibold">About Samaj</h3>
-
+<div class="wrapper">
+    <a href="javascript:window.history.back()"
+        class="inline-block mx-2 m-4  text-white bg-accent <hover:bg-accent-dark></hover:bg-accent-dark> focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 py-2 px-4 rounded-md">
+        <i class="fas fa-chevron-left"></i> Back
     </a>
-    <a href="<?= base_url('pad-adhikaari') ?>"
-        class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
-        <i class="fas fa-user-tie text-4xl"></i>
-        <h3 class="text-xl font-semibold">Pad Adhikaari</h3>
+    <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-1">
+        <a href="<?= base_url('about-samaj') ?>"
+            class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
+            <i class="fas fa-users text-4xl"></i>
+            <h3 class="text-xl font-semibold">About Samaj</h3>
 
-    </a>
-    <a href="<?= base_url('contact-admin') ?>"
-        class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
-        <i class="fas fa-envelope text-4xl"></i>
-        <h3 class="text-xl font-semibold">Contact Admin/Support</h3>
+        </a>
+        <a href="<?= base_url('pad-adhikaari') ?>"
+            class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
+            <i class="fas fa-user-tie text-4xl"></i>
+            <h3 class="text-xl font-semibold">Pad Adhikaari</h3>
 
-    </a>
-    <a href="<?= base_url('donation') ?>"
-        class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
-        <i class="fas fa-donate text-4xl"></i>
-        <h3 class="text-xl font-semibold">Donation</h3>
+        </a>
+        <a href="<?= base_url('contact-admin') ?>"
+            class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
+            <i class="fas fa-envelope text-4xl"></i>
+            <h3 class="text-xl font-semibold">Contact Admin/Support</h3>
 
-    </a>
-    <a href="<?= base_url('achievers') ?>"
-        class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
-        <i class="fas fa-trophy text-4xl"></i>
-        <h3 class="text-xl font-semibold">Achievers</h3>
+        </a>
+        <a href="<?= base_url('donation') ?>"
+            class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
+            <i class="fas fa-donate text-4xl"></i>
+            <h3 class="text-xl font-semibold">Donation</h3>
 
-    </a>
-    <a href="<?= base_url('news-and-events') ?>"
-        class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
-        <i class="fas fa-newspaper text-4xl"></i>
-        <h3 class="text-xl font-semibold">News & Events</h3>
+        </a>
+        <a href="<?= base_url('achievers') ?>"
+            class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
+            <i class="fas fa-trophy text-4xl"></i>
+            <h3 class="text-xl font-semibold">Achievers</h3>
 
-    </a>
+        </a>
+        <a href="<?= base_url('news-and-events') ?>"
+            class="flex flex-col items-center p-4 bg-white rounded shadow hover:scale-110 transition duration-300 ease-in-out hover:bg-accent hover:text-white">
+            <i class="fas fa-newspaper text-4xl"></i>
+            <h3 class="text-xl font-semibold">News & Events</h3>
+
+        </a>
+    </div>
 </div>
