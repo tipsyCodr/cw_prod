8b04051469c3fa5c09ea70872311b268b732ad9a
Hemant Kumar <itshemant566@gmail.com>
2024-09-18 15:46:50 +0530

    fixed verification form

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -192,10 +192,39 @@ class UserController extends CI_Controller
     public function verifySave()
     {
         $formData = $this->input->post();
-        $formData['user_id'] = $this->session->userdata('login');
-        $formData['status'] = 'pending';
-        // $formData['selfie'] = $this->input->post('selfie');
-        // $formData['aadhar_card'] = $this->input->post('aadhar_card');
+
+        $config['upload_path'] = './uploads/selfie/';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['max_size'] = '1000';
+        $config['max_width'] = '1024';
+        $config['max_height'] = '768';
+        $config['file_name'] = time() . '_' . $_FILES['selfie']['name'];
+
+        $this->upload->initialize($config);
+
+        if (!$this->upload->do_upload('selfie')) {
+            $error = $this->upload->display_errors();
+            $this->session->set_flashdata('error', 'Upload error : ' . $error);
+            $this->verifyForm();
+            return;
+        } else {
+            $uploadData = $this->upload->data();
+            $formData['selfie'] = $uploadData['file_name'];
+        }
+
+        $config['upload_path'] = './uploads/aadhar_card/';
+        $config['file_name'] = time() . '_' . $_FILES['aadhar_card']['name'];
+        $this->upload->initialize($config);
+
+        if (!$this->upload->do_upload('aadhar_card')) {
+            $error = $this->upload->display_errors();
+            $this->session->set_flashdata('error', 'Upload error : ' . $error);
+            $this->verifyForm();
+            return;
+        } else {
+            $uploadData = $this->upload->data();
+            $formData['aadhar_card'] = $uploadData['file_name'];
+        }
 
         $formData['created_on'] = date('Y-m-d H:i:s');
         $formData['updated_on'] = date('Y-m-d H:i:s');
