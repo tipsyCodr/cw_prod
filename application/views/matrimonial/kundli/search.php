035f54dbe0f8f756114da9ec1cd9e2b090919816
Hemant Kumar <itshemant566@gmail.com>
2024-09-27 14:04:15 +0530

    asd

--- application/views/matrimonial/kundli/search.php
+++ application/views/matrimonial/kundli/search.php
@@ -3,7 +3,7 @@
         <form class="" action="<?= base_url('kundli/result') ?>" method="post">
             <div class="sm:flex mx-auto justify-evenly  ">
                 <div class="boy bg-white  p-2 rounded-lg shadow shadow-blue-500 border border-blue-500 my-4 ">
-                    <h3 class="font-bold p-2">Enter Girl's Details</h3>
+                    <h3 class="font-bold p-2">Enter Boy's Details</h3>
                     <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
                     <input type="text" name="boy_name" id="name"
                         class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
