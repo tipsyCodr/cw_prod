eabed9a2573df84e96a08011cbb7de4e7e52d943
Hemant Kumar <itshemant566@gmail.com>
2024-10-01 19:04:03 +0530

    sad

--- application/controllers/MatrimonialController.php
+++ application/controllers/MatrimonialController.php
@@ -235,10 +235,10 @@ class MatrimonialController extends MY_Controller
 
         // Initialize Guzzle client
         $client = new \GuzzleHttp\Client();
-        echo 'boy: ';
-        var_dump($boy);
-        echo 'girl: ';
-        var_dump($girl);
+        // echo 'boy: ';
+        // var_dump($boy);
+        // echo 'girl: ';
+        // var_dump($girl);
         // Request for boy's coordinates
         $url = "https://geocode.maps.co/search?q=" . urlencode($boy['cities']) . "&api_key=66fba7903028b471845537pjwe8f2cc";
         $response = $client->request('GET', $url);
